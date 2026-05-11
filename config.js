5dd7baffcbd850af277b97737fc5f43e2f9fdc78
Sam <zw1503327@gmail.com>
2026-05-11 15:23:25 +0900

    add survey with images

---
+++ config.js
@@ -0,0 +1,436 @@
+var SURVEY_CONFIG = {
+  "cases": [
+    {
+      "caseNum": 1,
+      "objName": "cup",
+      "styleNum": 2,
+      "headerUrl": "images/headers/cup_2_header.jpg",
+      "methodOrder": {
+        "A": "styletex",
+        "B": "trellis",
+        "C": "seqtex",
+        "D": "ours"
+      },
+      "methods": {
+        "A": "images/exp/styletex/cup_2_4views_384size_pbr.jpg",
+        "B": "images/exp/trellis/cup_2_4views_384size_pbr.jpg",
+        "C": "images/exp/seqtex/cup_2_4views_384size_pbr.jpg",
+        "D": "images/exp/ours/cup_2_4views_384size_pbr.jpg"
+      }
+    },
+    {
+      "caseNum": 2,
+      "objName": "bones",
+      "styleNum": 1,
+      "headerUrl": "images/headers/bones_1_header.jpg",
+      "methodOrder": {
+        "A": "trellis",
+        "B": "seqtex",
+        "C": "styletex",
+        "D": "ours"
+      },
+      "methods": {
+        "A": "images/exp/trellis/bones_1_4views_384size_pbr.jpg",
+        "B": "images/exp/seqtex/bones_1_4views_384size_pbr.jpg",
+        "C": "images/exp/styletex/bones_1_4views_384size_pbr.jpg",
+        "D": "images/exp/ours/bones_1_4views_384size_pbr.jpg"
+      }
+    },
+    {
+      "caseNum": 3,
+      "objName": "shrimp",
+      "styleNum": 3,
+      "headerUrl": "images/headers/shrimp_3_header.jpg",
+      "methodOrder": {
+        "A": "seqtex",
+        "B": "styletex",
+        "C": "trellis",
+        "D": "ours"
+      },
+      "methods": {
+        "A": "images/exp/seqtex/shrimp_3_4views_384size_pbr.jpg",
+        "B": "images/exp/styletex/shrimp_3_4views_384size_pbr.jpg",
+        "C": "images/exp/trellis/shrimp_3_4views_384size_pbr.jpg",
+        "D": "images/exp/ours/shrimp_3_4views_384size_pbr.jpg"
+      }
+    },
+    {
+      "caseNum": 4,
+      "objName": "apple",
+      "styleNum": 2,
+      "headerUrl": "images/headers/apple_2_header.jpg",
+      "methodOrder": {
+        "A": "trellis",
+        "B": "styletex",
+        "C": "seqtex",
+        "D": "ours"
+      },
+      "methods": {
+        "A": "images/exp/trellis/apple_2_4views_384size_pbr.jpg",
+        "B": "images/exp/styletex/apple_2_4views_384size_pbr.jpg",
+        "C": "images/exp/seqtex/apple_2_4views_384size_pbr.jpg",
+        "D": "images/exp/ours/apple_2_4views_384size_pbr.jpg"
+      }
+    },
+    {
+      "caseNum": 5,
+      "objName": "buddhahead",
+      "styleNum": 1,
+      "headerUrl": "images/headers/buddhahead_1_header.jpg",
+      "methodOrder": {
+        "A": "seqtex",
+        "B": "trellis",
+        "C": "styletex",
+        "D": "ours"
+      },
+      "methods": {
+        "A": "images/exp/seqtex/buddhahead_1_4views_384size_pbr.jpg",
+        "B": "images/exp/trellis/buddhahead_1_4views_384size_pbr.jpg",
+        "C": "images/exp/styletex/buddhahead_1_4views_384size_pbr.jpg",
+        "D": "images/exp/ours/buddhahead_1_4views_384size_pbr.jpg"
+      }
+    },
+    {
+      "caseNum": 6,
+      "objName": "bear",
+      "styleNum": 3,
+      "headerUrl": "images/headers/bear_3_header.jpg",
+      "methodOrder": {
+        "A": "styletex",
+        "B": "seqtex",
+        "C": "trellis",
+        "D": "ours"
+      },
+      "methods": {
+        "A": "images/exp/styletex/bear_3_4views_384size_pbr.jpg",
+        "B": "images/exp/seqtex/bear_3_4views_384size_pbr.jpg",
+        "C": "images/exp/trellis/bear_3_4views_384size_pbr.jpg",
+        "D": "images/exp/ours/bear_3_4views_384size_pbr.jpg"
+      }
+    },
+    {
+      "caseNum": 7,
+      "objName": "shoe",
+      "styleNum": 2,
+      "headerUrl": "images/headers/shoe_2_header.jpg",
+      "methodOrder": {
+        "A": "trellis",
+        "B": "seqtex",
+        "C": "styletex",
+        "D": "ours"
+      },
+      "methods": {
+        "A": "images/exp/trellis/shoe_2_4views_384size_pbr.jpg",
+        "B": "images/exp/seqtex/shoe_2_4views_384size_pbr.jpg",
+        "C": "images/exp/styletex/shoe_2_4views_384size_pbr.jpg",
+        "D": "images/exp/ours/shoe_2_4views_384size_pbr.jpg"
+      }
+    },
+    {
+      "caseNum": 8,
+      "objName": "bottle",
+      "styleNum": 1,
+      "headerUrl": "images/headers/bottle_1_header.jpg",
+      "methodOrder": {
+        "A": "seqtex",
+        "B": "styletex",
+        "C": "trellis",
+        "D": "ours"
+      },
+      "methods": {
+        "A": "images/exp/seqtex/bottle_1_4views_384size_pbr.jpg",
+        "B": "images/exp/styletex/bottle_1_4views_384size_pbr.jpg",
+        "C": "images/exp/trellis/bottle_1_4views_384size_pbr.jpg",
+        "D": "images/exp/ours/bottle_1_4views_384size_pbr.jpg"
+      }
+    },
+    {
+      "caseNum": 9,
+      "objName": "shrimp",
+      "styleNum": 1,
+      "headerUrl": "images/headers/shrimp_1_header.jpg",
+      "methodOrder": {
+        "A": "styletex",
+        "B": "trellis",
+        "C": "seqtex",
+        "D": "ours"
+      },
+      "methods": {
+        "A": "images/exp/styletex/shrimp_1_4views_384size_pbr.jpg",
+        "B": "images/exp/trellis/shrimp_1_4views_384size_pbr.jpg",
+        "C": "images/exp/seqtex/shrimp_1_4views_384size_pbr.jpg",
+        "D": "images/exp/ours/shrimp_1_4views_384size_pbr.jpg"
+      }
+    },
+    {
+      "caseNum": 10,
+      "objName": "buddhahead",
+      "styleNum": 3,
+      "headerUrl": "images/headers/buddhahead_3_header.jpg",
+      "methodOrder": {
+        "A": "seqtex",
+        "B": "trellis",
+        "C": "styletex",
+        "D": "ours"
+      },
+      "methods": {
+        "A": "images/exp/seqtex/buddhahead_3_4views_384size_pbr.jpg",
+        "B": "images/exp/trellis/buddhahead_3_4views_384size_pbr.jpg",
+        "C": "images/exp/styletex/buddhahead_3_4views_384size_pbr.jpg",
+        "D": "images/exp/ours/buddhahead_3_4views_384size_pbr.jpg"
+      }
+    },
+    {
+      "caseNum": 11,
+      "objName": "apple",
+      "styleNum": 1,
+      "headerUrl": "images/headers/apple_1_header.jpg",
+      "methodOrder": {
+        "A": "trellis",
+        "B": "styletex",
+        "C": "seqtex",
+        "D": "ours"
+      },
+      "methods": {
+        "A": "images/exp/trellis/apple_1_4views_384size_pbr.jpg",
+        "B": "images/exp/styletex/apple_1_4views_384size_pbr.jpg",
+        "C": "images/exp/seqtex/apple_1_4views_384size_pbr.jpg",
+        "D": "images/exp/ours/apple_1_4views_384size_pbr.jpg"
+      }
+    },
+    {
+      "caseNum": 12,
+      "objName": "bones",
+      "styleNum": 3,
+      "headerUrl": "images/headers/bones_3_header.jpg",
+      "methodOrder": {
+        "A": "styletex",
+        "B": "seqtex",
+        "C": "trellis",
+        "D": "ours"
+      },
+      "methods": {
+        "A": "images/exp/styletex/bones_3_4views_384size_pbr.jpg",
+        "B": "images/exp/seqtex/bones_3_4views_384size_pbr.jpg",
+        "C": "images/exp/trellis/bones_3_4views_384size_pbr.jpg",
+        "D": "images/exp/ours/bones_3_4views_384size_pbr.jpg"
+      }
+    },
+    {
+      "caseNum": 13,
+      "objName": "cup",
+      "styleNum": 3,
+      "headerUrl": "images/headers/cup_3_header.jpg",
+      "methodOrder": {
+        "A": "seqtex",
+        "B": "styletex",
+        "C": "trellis",
+        "D": "ours"
+      },
+      "methods": {
+        "A": "images/exp/seqtex/cup_3_4views_384size_pbr.jpg",
+        "B": "images/exp/styletex/cup_3_4views_384size_pbr.jpg",
+        "C": "images/exp/trellis/cup_3_4views_384size_pbr.jpg",
+        "D": "images/exp/ours/cup_3_4views_384size_pbr.jpg"
+      }
+    },
+    {
+      "caseNum": 14,
+      "objName": "bear",
+      "styleNum": 1,
+      "headerUrl": "images/headers/bear_1_header.jpg",
+      "methodOrder": {
+        "A": "trellis",
+        "B": "seqtex",
+        "C": "styletex",
+        "D": "ours"
+      },
+      "methods": {
+        "A": "images/exp/trellis/bear_1_4views_384size_pbr.jpg",
+        "B": "images/exp/seqtex/bear_1_4views_384size_pbr.jpg",
+        "C": "images/exp/styletex/bear_1_4views_384size_pbr.jpg",
+        "D": "images/exp/ours/bear_1_4views_384size_pbr.jpg"
+      }
+    },
+    {
+      "caseNum": 15,
+      "objName": "shoe",
+      "styleNum": 1,
+      "headerUrl": "images/headers/shoe_1_header.jpg",
+      "methodOrder": {
+        "A": "styletex",
+        "B": "trellis",
+        "C": "seqtex",
+        "D": "ours"
+      },
+      "methods": {
+        "A": "images/exp/styletex/shoe_1_4views_384size_pbr.jpg",
+        "B": "images/exp/trellis/shoe_1_4views_384size_pbr.jpg",
+        "C": "images/exp/seqtex/shoe_1_4views_384size_pbr.jpg",
+        "D": "images/exp/ours/shoe_1_4views_384size_pbr.jpg"
+      }
+    },
+    {
+      "caseNum": 16,
+      "objName": "bottle",
+      "styleNum": 3,
+      "headerUrl": "images/headers/bottle_3_header.jpg",
+      "methodOrder": {
+        "A": "seqtex",
+        "B": "trellis",
+        "C": "styletex",
+        "D": "ours"
+      },
+      "methods": {
+        "A": "images/exp/seqtex/bottle_3_4views_384size_pbr.jpg",
+        "B": "images/exp/trellis/bottle_3_4views_384size_pbr.jpg",
+        "C": "images/exp/styletex/bottle_3_4views_384size_pbr.jpg",
+        "D": "images/exp/ours/bottle_3_4views_384size_pbr.jpg"
+      }
+    },
+    {
+      "caseNum": 17,
+      "objName": "apple",
+      "styleNum": 3,
+      "headerUrl": "images/headers/apple_3_header.jpg",
+      "methodOrder": {
+        "A": "trellis",
+        "B": "styletex",
+        "C": "seqtex",
+        "D": "ours"
+      },
+      "methods": {
+        "A": "images/exp/trellis/apple_3_4views_384size_pbr.jpg",
+        "B": "images/exp/styletex/apple_3_4views_384size_pbr.jpg",
+        "C": "images/exp/seqtex/apple_3_4views_384size_pbr.jpg",
+        "D": "images/exp/ours/apple_3_4views_384size_pbr.jpg"
+      }
+    },
+    {
+      "caseNum": 18,
+      "objName": "shrimp",
+      "styleNum": 2,
+      "headerUrl": "images/headers/shrimp_2_header.jpg",
+      "methodOrder": {
+        "A": "styletex",
+        "B": "seqtex",
+        "C": "trellis",
+        "D": "ours"
+      },
+      "methods": {
+        "A": "images/exp/styletex/shrimp_2_4views_384size_pbr.jpg",
+        "B": "images/exp/seqtex/shrimp_2_4views_384size_pbr.jpg",
+        "C": "images/exp/trellis/shrimp_2_4views_384size_pbr.jpg",
+        "D": "images/exp/ours/shrimp_2_4views_384size_pbr.jpg"
+      }
+    },
+    {
+      "caseNum": 19,
+      "objName": "bear",
+      "styleNum": 2,
+      "headerUrl": "images/headers/bear_2_header.jpg",
+      "methodOrder": {
+        "A": "seqtex",
+        "B": "styletex",
+        "C": "trellis",
+        "D": "ours"
+      },
+      "methods": {
+        "A": "images/exp/seqtex/bear_2_4views_384size_pbr.jpg",
+        "B": "images/exp/styletex/bear_2_4views_384size_pbr.jpg",
+        "C": "images/exp/trellis/bear_2_4views_384size_pbr.jpg",
+        "D": "images/exp/ours/bear_2_4views_384size_pbr.jpg"
+      }
+    },
+    {
+      "caseNum": 20,
+      "objName": "buddhahead",
+      "styleNum": 2,
+      "headerUrl": "images/headers/buddhahead_2_header.jpg",
+      "methodOrder": {
+        "A": "trellis",
+        "B": "seqtex",
+        "C": "styletex",
+        "D": "ours"
+      },
+      "methods": {
+        "A": "images/exp/trellis/buddhahead_2_4views_384size_pbr.jpg",
+        "B": "images/exp/seqtex/buddhahead_2_4views_384size_pbr.jpg",
+        "C": "images/exp/styletex/buddhahead_2_4views_384size_pbr.jpg",
+        "D": "images/exp/ours/buddhahead_2_4views_384size_pbr.jpg"
+      }
+    },
+    {
+      "caseNum": 21,
+      "objName": "bottle",
+      "styleNum": 2,
+      "headerUrl": "images/headers/bottle_2_header.jpg",
+      "methodOrder": {
+        "A": "styletex",
+        "B": "trellis",
+        "C": "seqtex",
+        "D": "ours"
+      },
+      "methods": {
+        "A": "images/exp/styletex/bottle_2_4views_384size_pbr.jpg",
+        "B": "images/exp/trellis/bottle_2_4views_384size_pbr.jpg",
+        "C": "images/exp/seqtex/bottle_2_4views_384size_pbr.jpg",
+        "D": "images/exp/ours/bottle_2_4views_384size_pbr.jpg"
+      }
+    },
+    {
+      "caseNum": 22,
+      "objName": "cup",
+      "styleNum": 1,
+      "headerUrl": "images/headers/cup_1_header.jpg",
+      "methodOrder": {
+        "A": "seqtex",
+        "B": "trellis",
+        "C": "styletex",
+        "D": "ours"
+      },
+      "methods": {
+        "A": "images/exp/seqtex/cup_1_4views_384size_pbr.jpg",
+        "B": "images/exp/trellis/cup_1_4views_384size_pbr.jpg",
+        "C": "images/exp/styletex/cup_1_4views_384size_pbr.jpg",
+        "D": "images/exp/ours/cup_1_4views_384size_pbr.jpg"
+      }
+    },
+    {
+      "caseNum": 23,
+      "objName": "bones",
+      "styleNum": 2,
+      "headerUrl": "images/headers/bones_2_header.jpg",
+      "methodOrder": {
+        "A": "trellis",
+        "B": "styletex",
+        "C": "seqtex",
+        "D": "ours"
+      },
+      "methods": {
+        "A": "images/exp/trellis/bones_2_4views_384size_pbr.jpg",
+        "B": "images/exp/styletex/bones_2_4views_384size_pbr.jpg",
+        "C": "images/exp/seqtex/bones_2_4views_384size_pbr.jpg",
+        "D": "images/exp/ours/bones_2_4views_384size_pbr.jpg"
+      }
+    },
+    {
+      "caseNum": 24,
+      "objName": "shoe",
+      "styleNum": 3,
+      "headerUrl": "images/headers/shoe_3_header.jpg",
+      "methodOrder": {
+        "A": "styletex",
+        "B": "seqtex",
+        "C": "trellis",
+        "D": "ours"
+      },
+      "methods": {
+        "A": "images/exp/styletex/shoe_3_4views_384size_pbr.jpg",
+        "B": "images/exp/seqtex/shoe_3_4views_384size_pbr.jpg",
+        "C": "images/exp/trellis/shoe_3_4views_384size_pbr.jpg",
+        "D": "images/exp/ours/shoe_3_4views_384size_pbr.jpg"
+      }
+    }
+  ]
+};
